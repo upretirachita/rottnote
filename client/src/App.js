b1dafeb14403c915ce9f9d7fd18c3b0c38a04ee0
Teemu <teemu.kotiharju@gmail.com>
2017-06-11 11:30:46 +0300

    Add an error message and troubleshooting instructions to the browser console when adding a new note fails due to server errror

--- client/src/App.js
+++ client/src/App.js
@@ -27,9 +27,15 @@ function addNote() {
 
 function sendNoteToServer(text) {
   const newNote = { text: text };
-  const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "notes", true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.send(JSON.stringify(newNote));
+  const xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = () => {
+      if ((xhr.readyState === 4) && (xhr.status >= 500)) {
+        console.log("Rottnote server error. Be sure to start the rottnote development environment by doing 'npm install' and 'npm start' at the TOP level! 'npm start' at TOP level will start webpack dev server to port 3000 and Node API backend to port 3001. NOTE: to avoid any CORS issues, the webpack dev server (at port 3000) is configured to proxy all requests to the '/notes' endpoint to the Node API backend (i.e. to port 3001)");
+      }
+  };
+  xhr.open("POST", "notes", true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.send(JSON.stringify(newNote));
 };
 export default App;
