7d8abfb1c16642114bd8e230ac35ffb9566cd11b
Ossi <op@lye.be>
2017-07-07 22:26:50 +0300

    Changed postNewNote to use fetch() instead of XMLHttpRequest()

--- client/src/RestApi.js
+++ client/src/RestApi.js
@@ -1,16 +1,13 @@
 const RestApi = {
   postNewNote(state) {
-
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = () => {
-        if ((xhr.readyState === 4) && (xhr.status >= 500)) {
-          console.log("Rottnote server error. Be sure to start the rottnote development environment by doing 'npm install' and 'npm start' at the TOP level! 'npm start' at TOP level will start webpack dev server to port 3000 and Node API backend to port 3001. NOTE: to avoid any CORS issues, the webpack dev server (at port 3000) is configured to proxy all requests to the '/notes' endpoint to the Node API backend (i.e. to port 3001)");
-        }
-    };
-    xhr.open("POST", "notes", true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.send(JSON.stringify(state));
+    fetch("notes", {
+      method: "POST",
+      headers: {
+        "Accept": "Application/json",
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(state)
+    });
   }
 }
 
