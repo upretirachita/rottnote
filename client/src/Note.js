e2dfae69c07237809251201a4c891b7ecb646325
Teemu Kotiharju <teemu.kotiharju@gmail.com>
2017-07-11 20:55:31 +0300

    Make Note editable and add edited text to Note component state

--- client/src/Note.js
+++ client/src/Note.js
@@ -4,6 +4,8 @@ class Note extends Component {
   // eslint-disable-next-line
   constructor(props) {
     super(props);
+
+    this.state = { text: this.props.note.text };
   }
 
 
@@ -11,7 +13,10 @@ class Note extends Component {
     if(this.props.modify)
       return(
         <div>
-          <input type="text" value={this.props.note.text}/>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={event => this.setState({ text: event.target.value})}/>
           <button
             type="button"
             onClick={() => this.props.onModify(this.props.index)}
@@ -26,15 +31,15 @@ class Note extends Component {
             type="button"
             onClick={() => this.props.onModify(this.props.index)}
             >Modify</button>
-            
+
             <button type="button"
             onClick={() => this.props.onSave(this.props.index)}
             >Save</button>
-            
+
           <button type="button"
             onClick={() => this.props.onDelete(this.props.index)}
             >Delete</button>
-            
+
         </div>
       );
   }
