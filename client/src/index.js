d3f951f28ef8c933a9ea269dc5db9384873c5489
Teemu Kotiharju <teemu.kotiharju@gmail.com>
2017-08-02 16:48:20 +0300

    Remove unregister call as as the service worker is already uninstalled

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+//import register, { unregister } from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
-//registerServiceWorker();
+//register();
+//unregister();
