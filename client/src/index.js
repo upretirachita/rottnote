6646bcd9e7bf0351b1381c02d0984894bffdf126
Teemu Kotiharju <teemu.kotiharju@gmail.com>
2017-08-02 15:38:34 +0300

    Remove ESLint warning

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker, { unregister } from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
